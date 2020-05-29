d611ce14870b856802c3a233c8a4d3b08ae3cea4
coderookie1994 <sharthak.ghosh@live.in>
2020-05-30 03:38:09 +0530

    Init commit with config and run commands

---
+++ microstack/CommandBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace microstack
+{
+    public class CommandBuilder
+    {
+        private CommandLineApplication _app;
+        public CommandBuilder()
+        {
+            _app = new CommandLineApplication();
+        }
+
+        public void ConfigureCommands()
+        {
+            _app.HelpOption(true);
+
+        }
+    }
+}
---
+++ microstack/Models/MicroStack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace microstack
+{
+    public class MicroStack
+    {
+        public List<Application> Apps { get; set; } 
+    }
+}
