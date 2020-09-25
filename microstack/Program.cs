f59a7d93f605d4d142afd4dac400a829e4ff97d6
coderookie1994 <sharthak.ghosh@live.in>
2020-09-25 22:06:17 +0530

    Remove commented code

--- microstack/Program.cs
+++ microstack/Program.cs
@@ -20,85 +20,15 @@ namespace microstack
     {
         static void Main(string[] args)
         {
-        //     var app = new CommandLineApplication()
-        //     {
-        //         Description = "Run your applications without using Visual Studio or manually running dotnet run",
-        //         Name = "microstack"
-        //     };
-
-        //     app.HelpOption(true);
-        //     app.Command("config", cmd =>
-        //     {
-        //         cmd.UnrecognizedArgumentHandling = UnrecognizedArgumentHandling.StopParsingAndCollect;
-
-        //         var configNameArg = cmd.Option("-n|--name <NAME>", "Configuration name for set of apps, name as .ms_<NAME>.json", CommandOptionType.SingleValue).IsRequired();
-
-        //         cmd.OnExecute(() =>
-        //         {
-        //             var configName = configNameArg.Value();
-
-        //             if (string.IsNullOrEmpty(configName))
-        //                 cmd.ShowHelp();
-
-        //             var directory = Directory.GetCurrentDirectory();
-
-        //             var filePath = Path.Combine(directory, $".ms_{configName}.json");
-        //             if (!File.Exists(filePath))
-        //             {
-        //                 using var sw = File.AppendText($".ms_{configName}.json");
-        //                 sw.WriteLine(ConfigurationTemplate.Template);
-        //             }
-
-        //             var processStartArgs = new ProcessStartInfo("notepad");
-        //             processStartArgs.Arguments = $"/A \"{filePath}\"";
-        //             using (var notepadProcess = Process.Start(processStartArgs))
-        //             {
-        //                 notepadProcess.WaitForExit();
-        //             }
-
-        //             return 1;
-        //         });
-        //     });
-
-        //     app.Command("run", (cmd) =>
-        //     {
-        //         cmd.OnExecute(() =>
-        //         {
-        //             var filename = Directory.GetFiles(Directory.GetCurrentDirectory()).FirstOrDefault(f => f.Contains(".ms_"));
-        //             if (string.IsNullOrEmpty(filename))
-        //             {
-        //                 using var sw = new StringWriter();
-        //                 sw.WriteLine("No config file found, run \"microstack config\" to setup initial configuration");
-
-        //                 cmd.Out = sw;
-        //                 return;
-        //             }
-
-        //             var appStack = JsonConvert.DeserializeObject<MicroStack>(File.ReadAllText(filename));
-        //         });
-        //     });
-
-        //     app.OnExecute(() =>
-        //     {
-        //         if(args.Length <= 0)
-        //             app.ShowHelp();
-
-        //         return 0;
-        //     });
-
-        //     app.UnrecognizedArgumentHandling = UnrecognizedArgumentHandling.CollectAndContinue;
-
-
-        //     app.Execute(args);
-        var cts = new CancellationTokenSource();
-        Host.CreateDefaultBuilder(args)
-            .ConfigureServices((context, services) => {
-                services.AddLogging();
-                services.AddSingleton<StackProcessor>();
-            })
-            .RunCommandLineApplicationAsync<MicroStack>(args, cts.Token)
-            .GetAwaiter()
-            .GetResult();
+            var cts = new CancellationTokenSource();
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) => {
+                    services.AddLogging();
+                    services.AddSingleton<StackProcessor>();
+                })
+                .RunCommandLineApplicationAsync<MicroStack>(args, cts.Token)
+                .GetAwaiter()
+                .GetResult();
         }
     }
 }
