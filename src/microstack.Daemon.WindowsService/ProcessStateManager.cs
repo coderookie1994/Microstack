3cba335b898bd9e4bee49158d63fb5229dd768e0
coderookie1994 <sharthak.ghosh@live.in>
2020-10-01 19:38:48 +0530

    Fix concurrency issue

--- src/microstack.Daemon.WindowsService/ProcessStateManager.cs
+++ src/microstack.Daemon.WindowsService/ProcessStateManager.cs
@@ -1,17 +1,24 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using System.Threading;
 
 namespace microstack.Daemon.WindowsService
 {
-    public class ProcessStateManager
+    public class ProcessStateManager : IDisposable
     {
-        public IList<ProcessState> _states;
+        public List<ProcessState> _states;
+        private BlockingCollection<int> _queueToRemove;
+        private Timer _timer;
+        private object _lockObj = new object();
         public ProcessStateManager()
         {
             _states = new List<ProcessState>();
+            _queueToRemove = new BlockingCollection<int>();
+            _timer = new Timer(RemoveState, null, 0, 5);
         }
 
         public void TerminateProcess(object sender, ProcessEventArgs args)
@@ -33,10 +40,15 @@ namespace microstack.Daemon.WindowsService
                     {
                         var process = Process.GetProcessById(processState.PID);
                         if (process != null)
+                        {
                             process.Kill(true);
+                            Console.WriteLine($"Terminated {pidInWmi}");
+                        }
                     }
                 }
-                _states.Remove(processState);
+                
+                var processStateIndex = _states.FindIndex(s => s.PID.Equals(args.PID));
+                _queueToRemove.Add(processStateIndex);                
             }
         }
 
@@ -45,6 +57,25 @@ namespace microstack.Daemon.WindowsService
             var state = new ProcessState(pid, microStackPid, ProcessMarkers.Green);
             state.TerminateProcess += TerminateProcess;
             _states.Add(state);
+        }
+
+        private void RemoveState(object state)
+        {
+            lock(_lockObj)
+            {
+                if (!_queueToRemove.IsAddingCompleted && _queueToRemove.Count > 0)
+                {
+                    var index = _queueToRemove.Take();
+                    if (index < _states.Count)
+                        _states.RemoveAt(index);
+                }
+            }
+            
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
         }
     }
 }
