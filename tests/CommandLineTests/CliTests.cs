1aec947a837d41aa8bac0d7aa0cf9dfaa069f83b
coderookie1994 <sharthak.ghosh@live.in>
2021-03-16 02:14:36 +0530

    restructure some tests and other files

---
+++ tests/CommandLineTests/CliTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microstack.CLI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microstack.Tests.CommandLineTests
+{
+    public class CliTests: WebApplicationFactory<Program>
+    {
+        
+    }
+}
