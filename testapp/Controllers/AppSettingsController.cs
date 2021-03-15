ede7430fc3e08f2bd52cbb5cea70985599b5bc8e
coderookie1994 <sharthak.ghosh@live.in>
2021-03-16 02:14:36 +0530

    restructure some tests and other files

---
+++ testapp/Controllers/AppSettingsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace testapp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppSettingsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly IConfiguration configuration;
+
+        public AppSettingsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), 200)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
+        public IActionResult Get(string id)
+        {
+            return Ok(this.configuration[id]);
+        }
+    }
+}
