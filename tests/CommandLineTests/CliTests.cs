018f6993d4f9e822f56c179a6cc262a7a25ccada
coderookie1994 <sharthak.ghosh@live.in>
2021-03-17 11:13:26 +0530

    cli tests in progress

--- tests/CommandLineTests/CliTests.cs
+++ tests/CommandLineTests/CliTests.cs
@@ -3,12 +3,73 @@ using Microsoft.Extensions.Hosting;
 using Microstack.CLI;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Microstack.Tests.CommandLineTests
 {
-    public class CliTests: WebApplicationFactory<Program>
+    public class CliTests : IClassFixture<IHttpClientFactory>, IDisposable
     {
-        
+        private bool disposedValue;
+        private CancellationTokenSource _cts;
+        private HttpClient _client;
+
+        public CliTests(IHttpClientFactory factory)
+        {
+            _cts = new CancellationTokenSource();
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ShouldOverrideConfigValues_WhenSpecifiedInConfiguration()
+        {
+            var cts = new CancellationTokenSource();
+            var testAdapter = new ProgramTest();
+            testAdapter.StartDaemon();
+            testAdapter.StartHost(new string[] { "run", "-v", "-c", Path.Combine("config", ".mstkc.json"), "-p", "profile2" }, cts);
+
+            Thread.Sleep(1000 * 5);
+
+            var response1 = await (await _client.GetAsync("https://localhost:5004/appsettings/ComplexObj:ComplexKey")).Content.ReadAsStringAsync();
+            var response2 = await (await _client.GetAsync("https://localhost:5005/appsettings/ComplexObj:ComplexKey")).Content.ReadAsStringAsync();
+
+            Assert.Equal("app1", response1);
+            Assert.Equal("app2", response2);
+            //cts.Cancel();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    _cts.Cancel();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~CliTests()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
--- tests/CommandLineTests/ProgramTest.cs
+++ tests/CommandLineTests/ProgramTest.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microstack.CLI;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 
 namespace Microstack.Tests.CommandLineTests
 {
-    class ProgramTest
+    public class ProgramTest
     {
+        public void StartHost(string[] args, CancellationTokenSource cts)
+        {
+            Program.CreateHostBuilder(args)
+                .RunCommandLineApplicationAsync<Microstack.CLI.Commands.MicroStack>(args, cts.Token);
+        }
+
+        public void StartDaemon()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Microstack.Daemon.WindowsService.Program.CreateHostBuilder(new string[] { }).Build().RunAsync();
+            }
+        }
     }
 }
